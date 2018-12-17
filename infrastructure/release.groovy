1bf83fc2dcbc63ae3fcc0db403690f5126f304b8
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-12-17 17:55:57 +0100

    add release.groovy

---
+++ infrastructure/release.groovy
@@ -0,0 +1,16 @@
+timestamps {
+        node {
+                stage('Setup') {
+                    checkout scm
+                }
+
+                stage('Release') {
+                       withCredentials([usernamePassword(
+                            credentialsId: 'github',
+                            passwordVariable: 'GIT_PASSWORD',
+                            usernameVariable: 'GIT_USERNAME')]) {
+                                sh """./mvnw -B release:prepare release:perform -Darguments=-Djvm="${env.JAVA_HOME_11}/bin/java -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_TAG}""""
+                       }
+                }
+            }
+}
